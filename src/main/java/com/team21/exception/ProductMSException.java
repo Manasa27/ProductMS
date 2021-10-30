f4c1cff39947f559414254581a753b41b3184139
Ritika <ritik@Desktop-09FNB4N>
2021-10-30 23:11:21 +0530

    Created Repositories and Exception class

---
+++ src/main/java/com/team21/exception/ProductMSException.java
@@ -0,0 +1,13 @@
+package com.team21.exception;
+
+public class ProductMSException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public ProductMSException(String message) {
+		super(message);
+	}
+}
---
+++ src/main/java/com/team21/repository/ProductRepository.java
@@ -0,0 +1,19 @@
+package com.team21.repository;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.team21.entity.ProductEntity;
+
+public interface ProductRepository extends CrudRepository<ProductEntity, String> {
+
+	public ProductEntity findByProdId(String id);
+
+	public ProductEntity findByProductName(String name);
+
+	public List<ProductEntity> findByCategory(String category);
+
+	public List<ProductEntity> findAll();
+
+}
